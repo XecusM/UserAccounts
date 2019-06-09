277b01bd2f52675ec47de38d169c3bea5e31e8b6
Xecus <abo.elfotouh@live.com>
2019-06-09 10:40:02 +0200

    finish frontend modification and create forms test

--- useraccounts/userprofile/tests.py
+++ useraccounts/userprofile/tests.py
@@ -1,9 +1,9 @@
 #userprofile tests.py
 from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
 from .models import User
-from .forms import (UserSignUp, UserProfileForm,
-                    FormChangePassword, FormRestPassword)
-
+from .forms import UserSignUp, UserProfileForm
+from . import views
 # Create your tests here.
 
 class TestModels(TestCase):
@@ -24,17 +24,17 @@ class TestModels(TestCase):
             password = 'testpassword12345'
         )
         # Test New SuperUser default values
-        self.assertTrue(True, self.SuperUser1.is_superuser)
-        self.assertTrue(True, self.SuperUser1.is_active)
-        self.assertTrue(True, self.SuperUser1.is_email_verified)
-        self.assertTrue(True, self.SuperUser1.is_staff)
+        self.assertTrue(self.SuperUser1.is_superuser)
+        self.assertTrue(self.SuperUser1.is_active)
+        self.assertTrue(self.SuperUser1.is_email_verified)
+        self.assertTrue(self.SuperUser1.is_staff)
 
     def test_create_user(self):
         '''
         This test method to test new user creation
         '''
         # Create New User
-        self.User1 = User.objects.create_superuser(
+        self.User1 = User.objects.create_user(
             username = 'xecusm',
             first_name = 'Mohamed',
             last_name = 'Aboel-fotouh',
@@ -42,7 +42,19 @@ class TestModels(TestCase):
             password = 'testpassword12345'
         )
         # Test New User default values
-        self.assertFalse(False, self.User1.is_superuser)
-        self.assertFalse(False, self.User1.is_active)
-        self.assertFalse(False, self.User1.is_email_verified)
-        self.assertFalse(False, self.User1.is_staff)
+        self.assertFalse(self.User1.is_superuser)
+        self.assertFalse(self.User1.is_active)
+        self.assertFalse(self.User1.is_email_verified)
+        self.assertFalse(self.User1.is_staff)
+
+class TestForms(TestCase):
+    '''
+    Class test for UserAccounts Forms
+    '''
+    def test_UserSignUp_fields(self):
+        form = UserSignUp()
+        expected = ['first_name', 'last_name',
+                    'email1', 'email2', 'username',
+                    'password1', 'password2']
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
